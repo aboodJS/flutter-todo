8dffb7c4543e7bc2b45e13e96e2f3da09e07b7dc
ajs <140010746+aboodJS@users.noreply.github.com>
2026-02-04 22:08:48 +0300

    added conditional rendering for the listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(MyApp());
 }
 
@@ -13,7 +13,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final myController = TextEditingController();
-  List<String> todos = [""];
+  List<String> todos = [];
 
   void addtask() {
     final text = myController.text;
@@ -42,21 +42,36 @@ class _MyAppState extends State<MyApp> {
                 IconButton.filled(onPressed: addtask, icon: Icon(Icons.send)),
               ],
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: todos.length,
-                prototypeItem: ListTile(title: Text(todos.first)),
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(textAlign: TextAlign.center, todos[index]),
-                  );
-                },
-              ),
-            ),
+            todos.length > 0
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: todos.length,
+                      prototypeItem: ListTile(title: Text(todos.first)),
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            textAlign: TextAlign.center,
+                            todos[index],
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                : Expanded(child: Center(child: Text("start adding tasks"))),
+
             BottomAppBar(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [Icon(Icons.task), Icon(Icons.add_task)],
+                children: [
+                  IconButton(
+                    onPressed: () => print("add tasks"),
+                    icon: Icon(Icons.add_task_outlined),
+                  ),
+                  IconButton(
+                    onPressed: () => print("Complete Tasks"),
+                    icon: Icon(Icons.check_circle_outline),
+                  ),
+                ],
               ),
             ),
           ],
