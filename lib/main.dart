f04460f823909003d2a362b5314fa57e47ddaf64
ajs <140010746+aboodJS@users.noreply.github.com>
2026-02-05 17:41:43 +0300

    added tab based navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,84 @@
+import 'package:flutter/material.dart';
 
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.list)),
+                Tab(icon: Icon(Icons.checklist)),
+              ],
+            ),
+          ),
+          body: TabBarView(children: [TaskScreen(), AboutPage()]),
+        ),
+      ),
+    );
+  }
+}
+
+class TaskScreen extends StatefulWidget {
+  const TaskScreen({super.key});
+
+  @override
+  State<TaskScreen> createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  final List<String> todos = [];
+  final myController = TextEditingController();
+  void addTodo() {
+    final text = myController.text;
+    setState(() {
+      todos.add(text);
+    });
+    myController.text = '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: 160, child: TextField(controller: myController)),
+              IconButton.filled(
+                onPressed: addTodo,
+                icon: const Icon(Icons.send),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) =>
+                  ListTile(title: Center(child: Text(todos[index]))),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("This is an about tab"));
+  }
+}
